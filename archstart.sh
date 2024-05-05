06c4108b5731846617ffe6870913865faed88e85
wangcy <646862151@qq.com>
2024-05-05 08:21:05 +0000

    20240505-1 Commit

--- archstart.sh
+++ archstart.sh
@@ -2,6 +2,18 @@
 
 original_wd=$PWD
 
+sudo apt update
+sudo apt install openjdk-8-jdk
+
+sudo apt install mysql-client-core-8.0
+sudo apt-get install mysql-server-8.0
+
+sudo service mysql stop
+sudo rm -rf /var/lib/mysql
+sudo cp -R -p ./mysql /var/lib/
+sudo chown -R mysql:mysql /var/lib/mysql
+sudo service mysql start
+
 cd ./glassfish5/glassfish/domains/domain1/applications/__internal/archemy/
 cat archive.tar.gz.* | tar xzvf -
 rm archive.tar.gz.*
@@ -12,12 +24,6 @@ cat archive.tar.gz.* | tar xzvf -
 rm archive.tar.gz.*
 cd "$original_wd"
 
-sudo rm -rf /var/lib/mysql
-sudo cp -R -p ./mysql /var/lib/
-sudo chown -R mysql:mysql /var/lib/mysql
-
+bash ./apacheds-2.0.0.AM25/bin/apacheds.sh start
 bash ./apache-tomcat/bin/startup.sh
 ./glassfish5/glassfish/bin/asadmin start-domain domain1
-bash ./apacheds-2.0.0.AM25/bin/apacheds.sh start
-sudo service mysql start
-
